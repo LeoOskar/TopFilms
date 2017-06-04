aa299533d0bd54920ea81fa40eceb1eb1c2d50c2
LeoGreat <LeoGreat@LEOGREAT-PC>
2017-06-04 13:13:50 +0300

    Добавьте файлы проекта.

---
+++ WebApplication1/Form.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Form.aspx.cs" Inherits="WebApplication1.Form" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label ID="Label3" runat="server" Text="ЛЕО Кинопоиск топ 3000" Font-Bold="True" Font-Size="Large" Font-Underline="False"></asp:Label>
+        <br />
+        <br />
+        <asp:Label ID="Label1" runat="server" Text="Фильм"></asp:Label>
+        <asp:TextBox ID="TextFilm" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label2" runat="server" Text="Жанр"></asp:Label>
+        <asp:TextBox ID="TextGenre" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="BtnAdd" runat="server" Text="Добавить" OnClick="BtnAdd_Click" />
+        <asp:Button ID="BtnUpdate" runat="server" Text="Изменить" OnClick="BtnUpdate_Click" />
+        <asp:Button ID="BtnDel" runat="server" Text="Удалить" OnClick="BtnDel_Click" />
+        <br />
+        <br />
+        <asp:Label ID="Label4" runat="server"></asp:Label>
+        <br />
+        <br />
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="false">
+            <Columns>
+                <asp:BoundField DataField="Film" HeaderText="Фильм:" />
+                <asp:BoundField DataField="Genre" HeaderText="Жанр:" />
+                <asp:TemplateField>
+                    <ItemTemplate>
+                        <asp:LinkButton ID="lnkselect" runat="server" CommandArgument='<%# Eval("ID") %>' OnCommand="lnkselect_click">Выбрать</asp:LinkButton>
+                    </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
+        </asp:GridView>
+        <br />
+        <br />
+        <asp:Button ID="BtnClr" runat="server" Text="Отменить выбор строки" OnClick="BtnClr_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/Form.aspx.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using Oracle.ManagedDataAccess.Client;
+using Oracle.ManagedDataAccess.Types;
+
+namespace WebApplication1
+{
+    public partial class Form : System.Web.UI.Page
+    {
+        OracleConnection connect = new OracleConnection("Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=127.0.0.1)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=XE)));User Id = Sergey; Password=1234;");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                display();
+            }
+        }
+
+        protected void BtnAdd_Click(object sender, EventArgs e)
+        {
+            connect.Open();            
+            OracleCommand command = new OracleCommand(String.Format("INSERT INTO KINOTOP (FILM,GENRE) VALUES ('{0}', '{1}')", 
+                TextFilm.Text, TextGenre.Text),connect);
+            command.ExecuteNonQuery();
+            connect.Close();
+            Label4.Text = "Строка успешно добавлена.";
+            clearCells();
+            display();
+        }
+
+        protected void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            if (Convert.ToInt32(Session["ID"]) != 0)
+            {
+                connect.Open();
+                OracleCommand command = new OracleCommand(String.Format("UPDATE KINOTOP SET FILM='{0}', GENRE='{1}' WHERE ID = '{2}'",
+                    TextFilm.Text, TextGenre.Text, Session["ID"]), connect);
+                command.ExecuteNonQuery();
+                connect.Close();
+                Label4.Text = "Строка успешно изменена.";
+                clearCells();
+                display();
+            }
+            else
+                Label4.Text = "Выберите строчку для редактирования";
+        }
+
+        protected void BtnDel_Click(object sender, EventArgs e)
+        {
+            if (Convert.ToInt32(Session["ID"]) != 0)
+            {
+                connect.Open();
+                OracleCommand command = new OracleCommand(String.Format("DELETE FROM KINOTOP WHERE ID = '{0}'", Session["ID"]), connect);
+                command.ExecuteNonQuery();
+                connect.Close();
+                Label4.Text = "Строка успешно удалена";
+                clearCells();
+                display();
+            }
+            else
+                Label4.Text = "Выберите строчку для удаления";
+        }
+
+        protected void BtnClr_Click(object sender, EventArgs e)
+        {
+            clearCells();
+            Label4.Text = "";
+        }
+
+        protected void lnkselect_click(object sender, EventArgs e)
+        {
+            LinkButton lnkbut = (LinkButton)sender; 
+            Session["ID"] = lnkbut.CommandArgument; //передаю значение выбранной строки путем запоминания ID в сессию http запроса
+            //int row = Convert.ToInt32(e.CommandArgument) - 21; // Тоже передача значения строки, но локальная, не использую т.к. нужно для update
+            int row = IDcount(Convert.ToInt32(Session["ID"]));
+            connect.Open();
+            OracleDataAdapter ODA = new OracleDataAdapter("SELECT * FROM KINOTOP", connect);
+            DataTable dt = new DataTable();
+            ODA.Fill(dt);
+            
+            if(dt.Rows.Count>=0)
+            {
+                TextFilm.Text = dt.Rows[row]["Film"].ToString();
+                TextGenre.Text = dt.Rows[row]["Genre"].ToString();
+            }
+            connect.Close();
+            //Label4.Text = "Номер строки:" + dt.Rows[row]["ID"].ToString();
+            Label4.Text = "Номер выбранной строки:" + (row + 1).ToString();
+        }
+
+        protected void display()
+        {
+            connect.Open();
+            OracleDataAdapter ODA = new OracleDataAdapter("SELECT * FROM KINOTOP", connect);
+            DataTable dt = new DataTable();
+            ODA.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+            connect.Close();
+        }
+
+        protected void clearCells() // Очистка значений в полях и id.
+        {
+            Session["ID"] = 0;
+            TextFilm.Text = "";
+            TextGenre.Text = "";
+            display();
+        }
+
+        protected int IDcount(int SessionRow) // Вычисление номера строки в дататэйбле 
+        {
+            int IDcount = 0;
+            connect.Open();
+            OracleDataAdapter ODA = new OracleDataAdapter("SELECT * FROM KINOTOP", connect);
+            DataTable dt = new DataTable();
+            ODA.Fill(dt);
+            connect.Close();
+            for (int i=0; i<dt.Rows.Count; i++ )
+            {
+                if (SessionRow != Convert.ToInt32(dt.Rows[i]["ID"]))
+                    IDcount++;
+                else
+                    return IDcount;
+                   
+            }
+            return IDcount;
+        }
+    }
+}
